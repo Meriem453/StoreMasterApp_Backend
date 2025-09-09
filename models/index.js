2d63166c6c7e13043e5da8ec5aacdbb7f572a09c
Meriem <mimi.04.05.2003@gmail.com>
2025-09-10 00:07:30 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -3,13 +3,9 @@ const dbConfig = require('../config/dbConfig.js');
 const {Sequelize, DataTypes} = require('sequelize');
 
 const sequelize = new Sequelize(
-    dbConfig.DB,
-    dbConfig.USER,
-    dbConfig.PASSWORD, {
-        host: dbConfig.HOST,
+    dbConfig.DB, {
         dialect: dbConfig.dialect,
         operatorsAliases: false,
-
         pool: {
             max: dbConfig.pool.max,
             min: dbConfig.pool.min,
